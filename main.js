6a56b8429a2089d6cf20803ce7124f4cafbb85d8
David Ankner <davidankner@MacBook-Air-som-tillhor-David.local>
2024-06-18 09:35:30 +0200

    Init JavaScript refresher

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("This is my first function");
+console.log("This is my second function");
+
+// alert window
+//window.alert("OMG kek");
+
+document.getElementById("MyH1").textContent = "Hello";
+document.getElementById("MyPG").textContent = "Paragraph Hello";
+
+//Declare variable, unique
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(` You are ${age} years old`)
+console.log(` And you cost about ${price}$`)
+console.log(` Your penis length is ${gpa}mm`)
+
